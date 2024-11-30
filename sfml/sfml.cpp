904f3b1356bd36c620319bb4bb453cbfba0b4553
Adrian Crainicu <crainicuadrian@gmail.com>
2024-11-30 17:30:29 +0500

    letter detection example

--- sfml/sfml.cpp
+++ sfml/sfml.cpp
@@ -31,6 +31,32 @@ int main()
 
     BAR::menu = new Menu(); // initialize it here not there
 
+    bool letter_detection = 1;
+    // exemplu la letter detection
+    // trb doar sa vezi daca mausu pica intre 2 litere sau e pe o litera, f ez
+    // vvvv
+    sf::Text text;
+    sf::RectangleShape rect;
+    if (letter_detection) {
+        float lineHeight;
+
+        text.setFont(font);
+        text.setCharacterSize(30);
+        text.setLineSpacing(1);
+        text.setString("qwertyuiopasdffghjklzxcvbnm|");
+        lineHeight = text.getGlobalBounds().height;
+        text.setString("Hello\nThis is another line of text\nByee\nn\nn\nn\nn\nn\nn\nn");
+
+        sf::Vector2f textSize;
+        textSize.x = text.getGlobalBounds().width;
+        textSize.y = text.getGlobalBounds().height;
+
+        rect.setPosition(text.getPosition());
+        rect.setSize(textSize);
+        rect.setFillColor(sf::Color::Red);
+    }
+    // ^^^^ 
+
     while (window.isOpen()) {
         sf::Event ev;
         while (window.pollEvent(ev)) { // todo wait ev ADRIAN NU UITA (don't waste CPU !! )
@@ -52,8 +78,39 @@ int main()
         BAR::menu->draw();
         draw_content(window);
 
+        // vvv si aici
+        if (letter_detection == 1) {
+            window.draw(rect);
+            for (int i = 0; i < text.getString().getSize(); i++) {
+                sf::FloatRect fr;
+                sf::Vector2f pos = text.findCharacterPos(i);
+                sf::Text t2 = text;
+                t2.setString(std::string("") + char(text.getString()[i]));
+                fr.width = t2.getGlobalBounds().width;
+                fr.height = t2.getGlobalBounds().height;
+                fr.left = pos.x;
+                fr.top = pos.y;
+
+                sf::RectangleShape r1;
+                pos += sf::Vector2f(text.getLetterSpacing(), text.getLineSpacing());
+                r1.setPosition(pos);
+                fr.height *= 2;
+                r1.setSize({ fr.width, fr.height });
+                r1.setFillColor(sf::Color::Green);
+                window.draw(r1);
+
+                if (r1.getGlobalBounds().contains(sf::Vector2f(sf::Mouse::getPosition(window)))) {
+                    std::cout << char(text.getString()[i]) << '\n';
+                }
+
+            }
+            window.draw(text);
+        }
+        // ^^^
+
         window.display();
     }
+   
 
     // works
 
